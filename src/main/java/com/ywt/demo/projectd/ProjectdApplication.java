fdcc8b6dc0a9b3bdf2b8c2a06c2e9212bf1bc69d
NeverMore93 <1305882275@qq.com>
2018-05-13 21:01:24 +0800

    Initial commit

---
+++ src/main/java/com/ywt/demo/projectd/ProjectdApplication.java
@@ -0,0 +1,12 @@
+package com.ywt.demo.projectd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectdApplication.class, args);
+	}
+}
